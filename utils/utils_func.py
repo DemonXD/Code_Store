f50e5473a7327e633d3a7020a6da3fd27190f387
Miles <kanonxmm@163.com>
2020-08-06 10:31:32 +0800

    add python print variable string name func

--- utils/utils_func.py
+++ utils/utils_func.py
@@ -2,6 +2,18 @@ import time
 import hashlib
 import pickle
 import functools
+import inspect
+import re
+
+def varname(p):
+    """
+    show variable string name
+    """
+    for line in inspect.getframeinfo(inspect.currentframe().f_back)[3]:
+        m = re.search(r'\bvarname\s*\(\s*([A-Za-z_][A-Za-z0-9_]*)\s*\)', line)
+        if m:
+            return m.group(1)
+
 
 def CMlist(list1, list2):
     l = len(list1)  + len(list2)
