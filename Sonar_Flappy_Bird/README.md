51bf088f12bf8bd4fba499d16e164c3cb40e5ca5
Miles <kanonxmm@163.com>
2020-08-14 17:20:49 +0800

    add sfml flappy bird sample

---
+++ Sonar_Flappy_Bird/README.md
@@ -0,0 +1,6 @@
+### Flappy Bird(C++)
+基于Github：SonarSystems的Flappy Bird教程，复写了一边  
+- Platform: Windows
+- Editor: VScode
+- SFML: v2.5.1
+- MingW64: 7.3.0
---
+++ Sonar_Flappy_Bird/SplashState.cpp
@@ -0,0 +1,44 @@
+#include <sstream>
+#include "SplashState.hpp"
+#include "MainMenuState.hpp"
+#include "DEFINITIONS.hpp"
+#include <iostream>
+
+namespace Sonar
+{
+    SplashState::SplashState(GameDataRef data): _data(data)
+    {
+    
+    }
+    void SplashState::Init()
+    {
+        _data->assets.LoadTexture("Splash State Backgroud",
+                                  SPLASH_SCENE_BACKGROUND_FILEPATH);
+        _background.setTexture(this->_data->assets.GetTexture("Splash State Backgroud"));
+    }
+
+    void SplashState::HandleInput()
+    {
+        sf::Event event;
+        while (_data->window.pollEvent(event))
+        {
+            if (sf::Event::Closed == event.type)
+            {
+                _data->window.close();
+            }
+        }
+    }
+    void SplashState::Update(float dt)
+    {
+        if (_clock.getElapsedTime().asSeconds() > SPLASH_STATE_SHOW_TIME)
+        {
+            _data->machine.AddState(StateRef(new MainMenuState(_data)), true);
+        }
+    }
+    void SplashState::Draw(float dt)
+    {
+        _data->window.clear();
+        _data->window.draw(_background);
+        _data->window.display();
+    }
+} // namespace Sonar
