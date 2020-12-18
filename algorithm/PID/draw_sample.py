2aca5fbabaeca24d739679d13d4c01b2f526eea7
DemonXD <kanonxmm@163.com>
2020-12-18 13:25:43 +0800

    fix(algorithm/PID/draw_sample.py): matplotlib 实时做图的sample

--- algorithm/PID/draw_sample.py
+++ algorithm/PID/draw_sample.py
@@ -1,12 +1,39 @@
+import time
+import numpy as np
 import matplotlib.pyplot as plt
+from threading import Thread
 
+
+count = 0
 ax = []                    # 定义一个 x 轴的空列表用来接收动态的数据
 ay = []                    # 定义一个 y 轴的空列表用来接收动态的数据
-plt.ion()                  # 开启一个画图的窗口
-for i in range(100):       # 遍历0-99的值
-	ax.append(i)           # 添加 i 到 x 轴的数据中
-	ay.append(i**2)        # 添加 i 的平方到 y 轴的数据中
-	plt.clf()              # 清除之前画的图
-	plt.plot(ax,ay)        # 画出当前 ax 列表和 ay 列表中的值的图形
-	plt.pause(0.001)         # 暂停一秒
-	plt.ioff()             # 关闭画图的窗口
+
+
+def draw():
+    global ax, ay
+    plt.ion()
+    plt.clf()
+    plt.plot(ax, ay)
+    plt.pause(0.001)
+    plt.ioff()
+    plt.draw()
+
+
+def generateData():
+    global count, ax, ay
+    while count < 100:
+        ax.append(count)
+        ay.append(np.sin(count%5))
+
+        count += 1
+        time.sleep(0.01)
+        draw()
+
+
+if __name__ == "__main__":
+    try:
+        generateData()
+        while 1:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        pass
